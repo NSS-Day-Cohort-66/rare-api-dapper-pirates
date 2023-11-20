73de960dbe1e13e458dd8fb0c31f12ca897dc3f1
Daniel Bennett <danielpsalmbennett@yahoo.com>
2023-11-20 12:53:21 -0600

    added regiter

--- rare/urls.py
+++ rare/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from rareapi.views.users import UserViewSet
+
+router = DefaultRouter(trailing_slash=False)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('register', UserViewSet.as_view({'post': 'register_account'}), name='register')
 ]
